21c79cc485c1a0840889a509ff91498bd445db57
Gabriel Robert <gabriel.robert@epitech.eu>
2024-06-11 00:24:54 +0200

    authenticate

---
+++ factory_app/src/app/login/page.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import { supabase } from "@/lib/supabase";
+import { useState } from "react";
+
+export default function Login() {
+    const [data, setData] = useState<{
+        email: string,
+        password: string
+    }>({
+        email: '',
+        password: ''
+    })
+    
+    const login = async () => {
+        try {                   
+        let { data, error } = await supabase.auth.signUp({
+        email: 'someone@email.com',
+        password: 'qAGfYerqOujWizOkukGy'
+        })
+
+            if (data) console.log(data); //redirect une fois connecté faire log in et faire une signup road pcq pr l'instant c en signup mais ca doit aller en log in une fois sign up sur auth supabase
+
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    const handleChange = (e: any) => {
+        const {name, value } = e.target;
+        setData((prev: any) => ({
+            ...prev,
+            [name]: value,
+        }));
+    }
+
+    return <div className="container mx-auto w-[400px] grid gap-4">
+        <div className='grid'>
+            <label>Email</label>
+            <input
+            type='text'
+            name='email'
+            value={data?.email}
+            onChange={handleChange}
+            />
+        </div>
+        <div className="container mx-auto w-[400px]">
+            <label>Password</label>
+            <input
+            type='password'
+            name='password'
+            value={data?.password}
+            onChange={handleChange}
+            />
+        </div>
+        <div>
+            <button className="px-4 py-2 bg-blue-500 rounded cursor-pointer" onClick={login}
+                >Sign up</button>
+        </div>
+    </div>;
+}
